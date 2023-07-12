73d06572d2d57dcd7be764afe37c553ef23c2fd9
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-07-12 12:13:57 -0700

    resolves

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,27 @@
 # your code goes here!
+import time
+
+
+def countdown():
+    i = 10
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    print("HAPPY NEW YEAR!")
+
+
+countdown()
+
+print("\n")
+
+
+def count_with_sleep():
+    i = 10
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+
+
+count_with_sleep()
